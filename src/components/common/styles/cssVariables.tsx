390cfaf16a89f6adae10760d72303ec5340b3205
SivaramPg <sivarampg95@gmail.com>
2021-09-23 17:34:00 +0530

    global styles

---
+++ src/components/common/styles/cssVariables.tsx
@@ -0,0 +1,13 @@
+import { css } from 'styled-components/macro';
+
+const cssVariables = css`
+  :root {
+    /* Font Families */
+
+    /* Color Palette */
+    --main-background-color: #ffffff;
+    --main-text-color: #000000;
+  }
+`;
+
+export { cssVariables };
